328acf2177218abec9fa45982c19bafdc816914b
leejungho <ljhmoa@gmail.com>
2023-01-30 08:56:45 +0900

    [Fix] swiper style 변경

--- client/js/main.js
+++ client/js/main.js
@@ -1,12 +1,10 @@
-const swiper = new Swiper(".first-swiper", {
-	slidesPerView: "4",
-	slidesPerGroup: "4",
-	pagination: {
-		el: ".swiper-pagination",
-		type: "fraction"
-	},
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev"
-	}
+const swiper = new Swiper('.first-swiper', {
+  slidesPerView: '4',
+  slidesPerGroup: '4',
+  // spaceBetween: '18px',
+  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
